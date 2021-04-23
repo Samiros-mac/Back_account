73a99e8da7bdff1bd4982a84e666cc91db93d986
Samir Babayev <samir.m.babayev@gmail.com>
2021-04-22 22:56:59 -0400

    first push

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World samir zaur");
+        System.out.println("Good evening samir hazar");
+        System.out.println("salam");
+    }
+}
